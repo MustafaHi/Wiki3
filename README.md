de4624fcef16612964faf8bf98f698b1360c4a7e
MustafaHi <matterik971@gmail.com>
2021-03-31 05:57:02 +0300

    Initial Source

---
+++ README.md
@@ -0,0 +1,24 @@
+
+# Doc3
+
+> Version : Alpha  
+> [Website / Demo / Guide](https://MustafaHi.github.io/Doc3/)  
+> [Change Log & Features](https://github.com/MustafaHi/Doc3/wiki/Change-log-&-Features)  
+> [Distribution / Release](/Distribution)  
+> [Developer Guide](https://github.com/MustafaHi/Doc3/wiki/Dev-Guide)  
+> [Contribution Guidelines](https://github.com/MustafaHi/Doc3/wiki/Dev-Guide)  
+> More information in the [wiki](https://github.com/MustafaHi/Doc3/wiki)  
+
+Doc3 is a documentation/knowledge base API allow for quick setup of wiki websites.  
+It requires no setup no building, just include and customize it to your liking.  
+Works with markdown to generate wiki.
+
+
+## Credit
+Doc3 @MIT licensed, by [@MustafaHi](https://github.com/MustafaHi)  
+current version use the following libraries: 
+[Marked](https://github.com/markedjs/marked)
+[Navigo](https://github.com/krasimir/navigo)
+[Zenscroll](https://github.com/zengabor/zenscroll)
+[PrismJS](https://github.com/PrismJS/prism)
+
---
+++ Source/doc3-setup.js
@@ -0,0 +1,61 @@
+const setupList = [{
+	t: "Getting Started",
+	c: [
+		{
+			t: "Installation",
+			l: "demo3.md"
+		},
+		{
+			t: "Upcoming Changes",
+			l: "demo2.md"
+		},
+		{
+			t: "Platform",
+			c: [
+				{ t: "Windows", l: "demo3.md" },
+				{ t: "Linux",   l: "demo4.md" }
+			]
+		},
+		{
+			t: "Latex",
+			l: "demo1.md"
+		}
+	]
+}, {
+	t: "Tables",
+	c: [{ t: "API", l: "demo2.md" }, { t: "All Documentations", l: "demo4.md" }]
+}];
+const guideList = [{
+	t: "Framework",
+	c: [
+		{ t: "Guide Lines", l: "demo2.md"},
+		{ t: "Setup", l: "demo3.md"},
+		{ t: "Colors", l: "demo4.html" },
+	]
+}, {
+	t: "Components",
+	c: [
+		{ t: "Grid", l: "demo1.md" },
+		{ t: "Headers", l: "demo4.md" },
+		{ t: "Buttons", l: "demo2.md" },
+		{ t: "Input", l: "demo3.md" },
+		{ t: "Checkboxes", l: "demo4.md" },
+	]
+}];
+
+
+
+const Setup = {
+	title: "Doc3 ",
+	path: "Doc3",
+	header: true,
+	search: true,
+	theme : true,
+	tracking: true,
+	codeTabs: true,
+	innerTable: true,
+	TableOfContent: true,
+	paragraphLink : true,
+	selectionLink : true,
+	pages: [["Main", "", setupList], ["Guide", "", guideList]]
+};
